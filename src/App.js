d2535e7717e9696d52a9586d54eb9f060c2722ca
Vishnu Shekhawat <vishnu.shekhawat@tothenew.com>
2016-10-20 18:23:00 +0530

    double-counter implemented

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
+import Counter from './components/counter'
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      count: 0
-    }
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick(){
-    this.setState({count:this.state.count+1})
-  }
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.handleClick}>{this.state.count}</button>
+  render(){
+    return(
+      <div>
+        <Counter/>
+        <Counter/>
       </div>
-    );
+    )
   }
 }
 
