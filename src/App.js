16e686580dd8faf478e98a13231bb31e8594ad32
Vishnu Shekhawat <vishnu.shekhawat@tothenew.com>
2016-10-20 18:30:23 +0530

    increment and decrement implemented

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import Counter from './components/counter'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 0
+    }
+    this.increase = this.increase.bind(this)
+    this.decrease = this.decrease.bind(this)
+  }
+  increase(){
+    this.setState({
+      count: this.state.count+1
+    })
+  }
+  decrease(){
+    this.setState({
+      count: this.state.count-1
+    })
+  }
   render(){
     return(
       <div>
-        <Counter/>
-        <Counter/>
+        <p><Counter handler={this.increase} btnText='Increase'/></p>
+        <p>{this.state.count}</p>
+        <Counter handler={this.decrease} btnText='Decrease'/>
       </div>
     )
   }
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,20 +1,10 @@
 import React, {Component} from 'react'
 
 export default class Counter extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      count: 0
-    }
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick(){
-    this.setState({count:this.state.count+1})
-  }
   render() {
     return (
       <div className="App">
-        <button onClick={this.handleClick}>{this.state.count}</button>
+        <button onClick={this.props.handler}>{this.props.btnText}</button>
       </div>
     );
   }
