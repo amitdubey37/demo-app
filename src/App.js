442fd236be2e9426a74fe4cb75d137edc0cd3a08
Vishnu Shekhawat <vishnu.shekhawat@tothenew.com>
2016-10-20 18:19:52 +0530

    single-counter implemented

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+    this.setState({count:this.state.count+1})
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.handleClick}>{this.state.count}</button>
       </div>
     );
   }
